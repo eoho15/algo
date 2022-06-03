cfa8ad29360949d7c584810cd93f71df51c0af17
Daeho <eoho115@naver.com>
2022-06-03 18:03:20 +0900

    Application(에러메세지)

--- src/webproject/Application.java
+++ src/webproject/Application.java
@@ -1,5 +1,8 @@
 package webproject;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 // Application의 객체를 만들어서 그걸 실행한다.
 public class Application {
 
@@ -16,12 +19,22 @@ public class Application {
             System.exit(EXIT_FAILURE);
         }
 
-        int port;
+        int port = 0;
         try {
             port = Integer.parseInt(args[0]);
         }catch( NumberFormatException e){
             System.out.println("에러 메세지"+ e);
             System.exit(EXIT_FAILURE);
+        }
+
+        try {
+            // Server 클래스를 사용하자
+            Server server = new Server(port);
+            server.run();
+        }catch(DaehoException e){
+            System.out.println("문제가 발생했습니다. "+ e.getMessage());
+        } catch(Throwable e){
+            System.out.println("예상치 못한 예외가 발생했습니다."+ e.getMessage());
         }
 
 

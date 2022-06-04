3929545321a4e20e16056d53696af4758fb11da6
Daeho <eoho115@naver.com>
2022-06-04 14:23:33 +0900

    Java Web Server 다시 연습

--- src/webproject/Application.java
+++ src/webproject/Application.java
@@ -1,45 +1,33 @@
 package webproject;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-
-// Application의 객체를 만들어서 그걸 실행한다.
 public class Application {
-
     private final String[] args;
     private static final int EXIT_FAILURE = 1;
-
     public Application(String[] args) {
         this.args = args;
     }
-
-    private void run() {
-        if(args.length <1){
-            System.out.println("인자의 갯수가 부족합니다.");
+    public void run() {
+        if(args.length < 1) {
+            System.out.println("인자개수가 부족합니드아아.");
+            //실패했을때
+            // 리눅스 시스템에서 0으로 종료하면 성공, 1로 종료하면 실패
             System.exit(EXIT_FAILURE);
+            // 실패할때 1로 리턴해주면 좋겠다..
+            // 매번 1로 할 수 없으니
         }
-
         int port = 0;
         try {
             port = Integer.parseInt(args[0]);
-        }catch( NumberFormatException e){
-            System.out.println("에러 메세지"+ e);
+        }catch(NumberFormatException e){
+            System.out.println("포트 번호가 잘 못 되었습니다.!!");
             System.exit(EXIT_FAILURE);
         }
 
-        try {
-            // Server 클래스를 사용하자
-            Server server = new Server(port);
-            server.run();
-        }catch(DaehoException e){
-            System.out.println("문제가 발생했습니다. "+ e.getMessage());
-        } catch(Throwable e){
-            System.out.println("예상치 못한 예외가 발생했습니다."+ e.getMessage());
-        }
+        Server server = new Server(port);
+        server.run();
 
 
-        }
-
+    }
     public static void main(String[] args) {
         Application application = new Application(args);
         application.run();
--- src/webproject/Server.java
+++ src/webproject/Server.java
@@ -1,74 +1,32 @@
 package webproject;
 
-import webproject.http.HttpMethod;
-import webproject.http.HttpVersion;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
     private final int port;
-    public Server(int port){
+
+    public Server(int port) {
         this.port = port;
     }
 
     public void run(){
-         try {
-             //port를 초기화 안했기 때문에 위에 int port; 에서 초기화를 해주어야 한다.
-             // 지정된 port로 server소켓을 연다.
-             ServerSocket serverSocket = new ServerSocket(port);
+        try {
+        ServerSocket serverSocket = new ServerSocket(port);
 
-             Socket socket = serverSocket.accept();
+        Socket socket = serverSocket.accept();
+        System.out.println(socket.getInputStream());
 
+        InputStream inputStream = socket.getInputStream();
+        inputStream.transferTo(System.out);
 
-             // new BufferedReader() 에 넣을 인자는 byte
+        socket.close();
 
-             // InputStreamReader에 넣을 인자는  bufferdInputStream
-             BufferedInputStream bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-             InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream);
-             // 아래와 같은 코드는 한줄씩 읽기 위해서 사용.
-             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-             // Header 부분은 위와 같이 처리하면 읽어진다
-
-             // Body부분은 조금 더 구체적인 로직이 요구된다.
-
-
-             // bufferedReader로 첫번째 줄을 읽어보자.
-             String startLine = bufferedReader.readLine();
-             // "GET / HTTP / 1.1";
-             String[] token = startLine.split(" ", 3);
-             if(token.length < 3){
-
-                 throw new HttpRequsetParsingFailedException("잘못된 start line이 들어왔습니다.");
-              }
-
-             String methodString = token[0];
-             String requestTarget = token[1];
-             String versionString = token[2];
-
-             HttpMethod method;
-             try {
-                 method = HttpMethod.valueOf(methodString);
-             } catch(IllegalArgumentException e){
-                throw new HttpRequsetParsingFailedException("잘못된 method가 들어왔습니다.",e);
-             }
-
-             if(versionString.equals("HTTP/1.1")){
-                 System.out.println("버전이 1.1이 아닙니다.");
-                throw new HttpRequsetParsingFailedException("버전이 1.1이 아닙니다.");
-             }
-
-             HttpVersion version = HttpVersion.VERSION_1_1;
-
-
-
-             socket.close();
-
-         }catch(IOException e){
-            throw new DaehoException("IO 문제가 발생했습니다,", e);
-         }
-     }
+    }catch (IOException ioException) {
+            System.out.println("IO 문제가 발생했습니다.");
+        }
+    }
 }

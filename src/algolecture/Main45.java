6cae7d182b52a716a48ec0efa60590e68fafc579
Daeho <eoho115@naver.com>
2022-06-13 15:36:16 +0900

    정렬하기(2) Bubble 정렬

---
+++ src/algolecture/Main45.java
@@ -0,0 +1,31 @@
+package algolecture;
+
+import java.util.Scanner;
+
+public class Main45 {
+    public int[] solution(int n , int[] arr){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main45 T = new Main45();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<arr.length;i++)
+            arr[i] = sc.nextInt();
+        for(int x : T.solution(n,arr))
+            System.out.print(x + " ");
+
+
+    }
+}

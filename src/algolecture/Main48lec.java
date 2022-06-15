25645b01d9d051b8e8391b2f0699e24e7d95cf21
Daeho <eoho115@naver.com>
2022-06-15 13:11:43 +0900

    장난꾸러기(1) -강의 코드

---
+++ src/algolecture/Main48lec.java
@@ -0,0 +1,34 @@
+package algolecture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main48lec {
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        // 깊은복사를 해야한다!
+        // 얕은 복사를 하면 안된다!
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+        for(int i=0;i<n;i++){
+            if(arr[i] != tmp[i]){
+                answer.add(i+1);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main48lec T = new Main48lec();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+            arr[i] = kb.nextInt();
+        for(int x : T.solution(n,arr))
+            System.out.print(x + " ");
+    }
+}

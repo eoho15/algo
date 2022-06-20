d6e24728d10579ab5b1d1bb3ad58c11c7a73b9d0
Daeho <eoho115@naver.com>
2022-06-21 02:17:12 +0900

    채팅프로그램!!

---
+++ src/chat2/ChatServer.java
@@ -0,0 +1,22 @@
+package chat2;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ChatServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8888);
+
+        // 동시성 문제를 해결하기 위해서..!
+        List<ChatThread> list = Collections.synchronizedList(new ArrayList<>());
+
+        while(true) {
+            Socket socket = serverSocket.accept();
+            ChatThread chatClient = new ChatThread(socket, list);
+            chatClient.start();
+        }
+    }
+}
